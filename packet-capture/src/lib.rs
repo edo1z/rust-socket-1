c53f92b94820b35dede0d9c78411c0742592d317
web3ten0 <web3ten0@gmail.com>
2022-07-16 22:06:35 +0900

    受信データのethertype表示

--- packet-capture/src/lib.rs
+++ packet-capture/src/lib.rs
@@ -1,22 +1,37 @@
-use pnet::datalink;
+use pnet::datalink::{self, Channel::Ethernet, NetworkInterface};
+use pnet::packet::ethernet::{EthernetPacket, MutableEthernetPacket};
 use std::{error::Error, io};
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 pub fn capture() -> Result<()> {
     println!("\nPACKET CAPTURE\n");
+    let interface = select_network_interface()?;
+    let channel = datalink::channel(&interface, Default::default());
+    let (mut tx, mut rx) = match channel {
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("Unhandled channel type"),
+        Err(e) => panic!("{}", e),
+    };
+    loop {
+        match rx.next() {
+            Ok(packet) => {
+                let packet = EthernetPacket::new(packet).unwrap();
+                println!("{}", packet.get_ethertype());
+            }
+            Err(e) => {
+                panic!("ERROR! {}", e);
+            }
+        }
+    }
+}
+
+fn select_network_interface() -> Result<NetworkInterface> {
     let interfaces = datalink::interfaces();
     println!("{} Network Interfaces", interfaces.len());
     for ni in &interfaces {
         println!("--------------------");
         println!("{}: {:?}", ni.index, ni.name);
-        // let mac = match ni.mac {
-        //     Some(mac) => format!("{}", mac),
-        //     None => String::from(""),
-        // };
-        // println!("mac: {}", mac);
-        // println!("ips: {:?}", ni.ips);
-        // println!("flags {:?}", ni.flags);
     }
     println!("--------------------");
     println!(
@@ -32,6 +47,5 @@ pub fn capture() -> Result<()> {
         .unwrap();
 
     println!("You choiced {}.", interface.name);
-
-    Ok(())
+    Ok(interface)
 }
