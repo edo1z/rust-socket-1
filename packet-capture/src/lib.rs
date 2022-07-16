8de58963b01c261da175facbd47f25fb795c2e61
web3ten0 <web3ten0@gmail.com>
2022-07-16 19:10:12 +0900

    NetworkInterfaceの表示

--- packet-capture/src/lib.rs
+++ packet-capture/src/lib.rs
@@ -1,8 +1,22 @@
+use pnet::datalink;
 use std::error;
 
 type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
 pub fn capture() -> Result<()> {
     println!("packet capture");
+    let interfaces = datalink::interfaces();
+    println!("{} Network Interfaces", interfaces.len());
+    for ni in interfaces {
+        println!("--------------------");
+        println!("{}: {:?}", ni.index, ni.name);
+        let mac = match ni.mac {
+            Some(mac) => format!("{}", mac),
+            None => String::from(""),
+        };
+        println!("mac: {}", mac);
+        println!("ips: {:?}", ni.ips);
+        println!("flags {:?}", ni.flags);
+    }
     Ok(())
 }
