d01c65d2003b8bbcbc16c61cb8b04dd1737c6d73
web3ten0 <web3ten0@gmail.com>
2022-07-16 14:37:08 +0900

    udpサーバ

--- udp-server/src/lib.rs
+++ udp-server/src/lib.rs
@@ -1,7 +1,16 @@
 use std::error;
+use std::net::UdpSocket;
+use std::str;
+
 type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
 pub fn serve(address: &str) -> Result<()> {
-    println!("UDP SERVER {}", address);
-    Ok(())
+    let server_socket = UdpSocket::bind(address)?;
+    loop {
+        let mut buf = [0u8; 1024];
+        let (size, src) = server_socket.recv_from(&mut buf)?;
+        println!("Handling data from {}", src);
+        println!("{}", str::from_utf8(&buf[..size])?);
+        server_socket.send_to(&buf, src)?;
+    }
 }
