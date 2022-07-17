fe0c9fe16c29dba7d543dc91850225c0ca1fb557
web3ten0 <web3ten0@gmail.com>
2022-07-17 12:59:06 +0900

    パケットのバイト列表示

--- packet-capture/src/lib.rs
+++ packet-capture/src/lib.rs
@@ -1,5 +1,11 @@
 use pnet::datalink::{self, Channel::Ethernet, NetworkInterface};
-use pnet::packet::ethernet::{EthernetPacket, MutableEthernetPacket};
+use pnet::packet::{
+    ethernet::{EtherTypes, EthernetPacket},
+    ip::IpNextHeaderProtocols,
+    ipv4::Ipv4Packet,
+    ipv6::Ipv6Packet,
+    Packet,
+};
 use std::{error::Error, io};
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
@@ -17,13 +23,60 @@ pub fn capture() -> Result<()> {
         match rx.next() {
             Ok(packet) => {
                 let packet = EthernetPacket::new(packet).unwrap();
-                println!("{}", packet.get_ethertype());
+                println!("{:?}", packet);
+                let ethertype = packet.get_ethertype();
+                println!("{}", ethertype);
+                match ethertype {
+                    EtherTypes::Ipv4 => {
+                        ipv4_handler(&packet);
+                    }
+                    EtherTypes::Ipv6 => {
+                        ipv6_handler(&packet);
+                    }
+                    _ => {}
+                }
             }
             Err(e) => {
                 panic!("ERROR! {}", e);
             }
         }
     }
+}
+
+fn ipv4_handler(ethernet_packet: &EthernetPacket) {
+    let payload = ethernet_packet.payload();
+    if let Some(packet) = Ipv4Packet::new(payload) {
+        match packet.get_next_level_protocol() {
+            IpNextHeaderProtocols::Tcp => {
+                tcp_handler(packet.payload());
+            }
+            IpNextHeaderProtocols::Udp => {
+                udp_handler(packet.payload());
+            }
+            _ => {}
+        }
+    }
+}
+
+fn ipv6_handler(ethernet_packet: &EthernetPacket) {
+    if let Some(packet) = Ipv6Packet::new(ethernet_packet.payload()) {
+        match packet.get_next_header() {
+            IpNextHeaderProtocols::Tcp => {
+                tcp_handler(packet.payload());
+            }
+            IpNextHeaderProtocols::Udp => {
+                udp_handler(packet.payload());
+            }
+            _ => {}
+        }
+    }
+}
+
+fn tcp_handler(packet: &[u8]) {
+    println!("tcp {:?}", packet);
+}
+fn udp_handler(packet: &[u8]) {
+    println!("udp {:?}", packet);
 }
 
 fn select_network_interface() -> Result<NetworkInterface> {
